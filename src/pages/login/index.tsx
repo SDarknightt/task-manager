16315546e36e4f5e27afda9ede8258809d3a7782
Samuel Dias Oliveira <114256690+SDarknightt@users.noreply.github.com>
2024-03-13 00:18:07 -0300

    feat: implementação tela de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,10 @@ import {getCsrfToken, signIn, signOut, useSession} from "next-auth/react";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import {useEffect, useState} from "react";
 import { useRouter } from "next/router";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "~/components/ui/card";
+import {Button} from "~/components/ui/button";
+import {FaDiscord} from "react-icons/fa";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "~/components/ui/carousel";
 
 export default function AuthHandler({children}: any, {csrfToken}: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const session = useSession();
@@ -9,22 +13,23 @@ export default function AuthHandler({children}: any, {csrfToken}: InferGetServer
     const [shouldLogout, setShouldLogout] = useState(false);
     const [counter, setCounter] = useState(5);
 
-    useEffect(() => {
-        if (session.status === 'unauthenticated') {
-            const handleSignIn = async () => {
-                try {
-                    const response = await signIn("discord");
-                    if (response?.ok) {
-                        void router.push("/home");
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-            };
-
-            void handleSignIn();
-        }
-    }, [session, router]);
+    //removido para o login discord ser direcionado pela pagina de login da aplicação
+    // useEffect(() => {
+    //     if (session.status === 'unauthenticated') {
+    //         const handleSignIn = async () => {
+    //             try {
+    //                 const response = await signIn("discord");
+    //                 if (response?.ok) {
+    //                     void router.push("/home");
+    //                 }
+    //             } catch (error) {
+    //                 console.error(error);
+    //             }
+    //         };
+    //
+    //         void handleSignIn();
+    //     }
+    // }, [session, router]);
 
     useEffect(() => {
         if (session.status === 'authenticated') {
@@ -55,8 +60,9 @@ export default function AuthHandler({children}: any, {csrfToken}: InferGetServer
             </div>
         );
     }
-
-    return null;
+    return (
+        SignInPage()
+    );
 }
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     return {
@@ -64,4 +70,51 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
             csrfToken: await getCsrfToken(context),
         },
     }
+}
+
+
+
+export function SignInPage(){
+    return (
+        <main className="h-screen flex w-full">
+            <div className="bg-primary-foreground w-full h-full flex p-16 items-center justify-center">
+                <Carousel className="w-full max-w-xl">
+                    <CarouselContent>
+                        <CarouselItem>
+                            <div className="flex aspect-square rounded p-8">
+                                <img src="/assets/amico.svg" alt="Imagem Amico" className="w-full h-full" />
+                            </div>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <div className="flex aspect-square rounded p-8">
+                                <img src="/assets/bro.svg" alt="Imagem Amico" className="w-full h-full" />
+                            </div>
+                        </CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious/>
+                    <CarouselNext/>
+                </Carousel>
+            </div>
+            <section className="flex items-center justify-center bg-background h-full max-w-3xl w-full p-4">
+                <Card className="w-full max-w-md">
+                    <CardHeader>
+                        <CardTitle className="text-2xl font-bold tracking-tighter flex items-center">
+                            <span className="mr-3">Task Manager</span>
+                        </CardTitle>
+                        <CardDescription>
+                            Utilize a sua conta do Discord para entrar na aplicação.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Button className="mt-1 w-full" onClick={() => signIn('discord')}>
+                           <FaDiscord className="mr-2"/> Entrar com Discord
+                        </Button>
+                    </CardContent>
+                    <CardFooter>
+                        <p className="text-muted-foreground text-center text-sm">Ao entrar na nossa plataforma você concorda com os nossos Termos de Uso e Política de Privacidade</p>
+                    </CardFooter>
+                </Card>
+            </section>
+        </main>
+    );
 }
