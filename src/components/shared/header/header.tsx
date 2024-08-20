31aa727b516d1cf5057e8a39b062c8f0d678297f
SDarknightt <samueldias004@gmail.com>
2024-08-19 21:09:53 -0300

    feat: implementação dropdown-menu.tsx no header

--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -1,8 +1,38 @@
-import {Moon, PanelLeftClose, PanelLeftOpen, Sun} from "lucide-react";
+import {
+    LogOut,
+    Moon,
+    PanelLeftClose,
+    PanelLeftOpen,
+    Sun, User,
+    MenuIcon, Share2
+} from "lucide-react";
 import {useEffect, useState} from "react";
 import {useTheme} from "next-themes";
 import { Menubar } from "~/components/ui/menubar";
 import Image from "next/image";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu"
+import {Button} from "~/components/ui/button";
+import {useSession} from "next-auth/react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription, DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "~/components/ui/dialog";
+import {DialogBody} from "next/dist/client/components/react-dev-overlay/internal/components/Dialog";
+import {Avatar, AvatarFallback, AvatarImage} from "~/components/ui/avatar";
+import {api} from "~/utils/api";
+
+import * as React from "react";
 
 interface HeaderProps {
     isSidebarOpen: boolean;
@@ -21,15 +51,15 @@ export default function Header({ isSidebarOpen, toggleSidebar }: HeaderProps) {
         <Menubar className="menu-header rounded-none px-2 border-l-0 border-r-0 border-t-0 border-b  lg:px-4 p-8">
             <div className="flex justify-start">
                 <div className="flex flex-row self-center ml-2">
-                        <Image
-                            src="/assets/logo.svg"
-                            width={80}
-                            height={0}
-                            style={{filter: filter}}
-                            alt="Logo KanPlan"
-                            className="ml-3"
-                        />
-                    </div>
+                    <Image
+                        src="/assets/logo.svg"
+                        width={80}
+                        height={0}
+                        style={{filter: filter}}
+                        alt="Logo KanPlan"
+                        className="ml-3"
+                    />
+                </div>
             </div>
             <div className="flex justify-end align-end">
                 <button onClick={toggleSidebar} className="p-2 top-4 left-4 z-50">
@@ -39,15 +69,151 @@ export default function Header({ isSidebarOpen, toggleSidebar }: HeaderProps) {
                         <PanelLeftOpen className="transition-transform duration-300 transform rotate-0"/>
                     )}
                 </button>
-                <button className="flex items-center pl-2"
-                        onClick={(): void => setTheme(theme === 'dark' ? 'light' : 'dark')} suppressHydrationWarning>
-                    {theme == "dark" ? (
-                        <Moon className="transition-transform duration-300 transform rotate-180"/>
-                    ) : (
-                        <Sun className="transition-transform duration-300 transform rotate-0"/>
-                    )}
-                </button>
+                <DropdownHeaderItens/>
             </div>
         </Menubar>
+    );
+}
+
+
+function DropdownHeaderItens() {
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+    const handleDialogOpen = () => {
+        setIsDialogOpen(true);
+    };
+
+    const handleDialogClose = () => {
+        setIsDialogOpen(false);
+    };
+
+    const handleShareDialogOpen = () => {
+        setIsShareDialogOpen(true);
+    };
+
+    const handleShareDialogClose = () => {
+        setIsShareDialogOpen(false);
+    };
+
+    const toggleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    };
+
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline"><MenuIcon/></Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem onClick={handleDialogOpen}>
+                            <Button variant="ghost">
+                                <User className="mr-2 h-4 w-4"/>
+                                <span>Perfil</span>
+                            </Button>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={handleShareDialogOpen}>
+                            <Button variant="ghost">
+                                <Share2 className="mr-2 h-4 w-4"/>
+                                <span>Código de Convite</span>
+                            </Button>
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem onClick={toggleTheme}>
+                            <Button variant="ghost">
+                                {theme === 'dark' ? (
+                                    <Sun className="mr-2 h-4 w-4"/>
+                                ) : (
+                                    <Moon className="mr-2 h-4 w-4"/>
+                                )}
+                                <span>{theme === 'dark' ? 'Claro' : 'Noturno'}</span>
+                            </Button>
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className={"text-red-500"}>
+                        <LogOut className=" mr-2 h-4 w-4" />
+                        <span>Sair</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            {isDialogOpen && <DialogUserItem onClose={handleDialogClose} />}
+            {isShareDialogOpen && <DialogShareId onClose={handleShareDialogClose} />}
+        </>
+    );
+}
+
+interface DialogUserItemProps {
+    onClose: () => void;
+}
+
+export function DialogUserItem({ onClose }: DialogUserItemProps) {
+    const { data: sessionData } = useSession();
+
+    return (
+        <Dialog open={true} onOpenChange={onClose}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Perfil</DialogTitle>
+                    <DialogBody>
+                        <div className="flex flex-col items-center">
+                            <div className="justify-self-center">
+                                <Avatar className={"min-h-[80px] min-w-[80px]"}>
+                                    <AvatarImage className="rounded-[40px] min-h-2 min-w-2 object-cover"
+                                                 src={sessionData?.user?.image ? sessionData?.user?.image : undefined}/>
+                                    <AvatarFallback>{sessionData?.user?.name?.charAt(0)}</AvatarFallback>
+                                </Avatar>
+                            </div>
+                            <h4 className="font-bold text-lg mt-5"> {sessionData?.user?.name}</h4>
+                            <h3 className="font-bold">{sessionData?.user?.email}</h3>
+                        </div>
+                    </DialogBody>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+interface DialogShareIdProps {
+    onClose: () => void;
+}
+
+export function DialogShareId({ onClose }: DialogShareIdProps) {
+    const [shareId, setShareId] = useState(null as unknown as number);
+    const userMutation = api.user.generateShareId.useMutation();
+
+    useEffect(() => {
+        void userMutation.mutateAsync({}).then(id => {
+            if(id){
+                setShareId(id);
+            }
+        });
+        return () => {
+            setShareId(null as unknown as number);
+        }
+    }, []);
+
+    return (
+        <Dialog open={true} onOpenChange={onClose}>
+            <DialogContent className="flex-col flex items-center">
+                <DialogHeader className="flex-col flex items-center">
+                    <DialogTitle>Código de convite</DialogTitle>
+                    <DialogDescription className="flex-col flex items-center">
+                        Utilize esse código para estabeler conexão com outros usuários.
+                        <h1 className="font-bold text-xl">{shareId}</h1>
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <Button onClick={onClose}>Fechar</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     );
 }
--- src/components/shared/user-item/user-item.tsx
+++ src/components/shared/user-item/user-item.tsx
@@ -1,35 +1,13 @@
 'use client';
 import {Avatar, AvatarFallback, AvatarImage} from "~/components/ui/avatar";
-import {signOut, useSession} from "next-auth/react";
-import {useEffect, useState} from "react";
-import {Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle} from "~/components/ui/dialog";
+import {useSession} from "next-auth/react";
 import * as React from "react";
-import {Button} from "~/components/ui/button";
-import {
-    AlertDialog, AlertDialogAction,
-    AlertDialogContent, AlertDialogDescription, AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger
-} from "~/components/ui/alert-dialog";
-import {api} from "~/utils/api";
-import {DialogBody} from "next/dist/client/components/react-dev-overlay/internal/components/Dialog";
-import {LogOut} from "lucide-react";
 
 export default function UserItem(){
     const { data: sessionData } = useSession();
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleClose = () => {
-        setIsOpen(false);
-    }
-
-    const handleOpen = () => {
-        setIsOpen(true);
-    }
 
     return (
-        <div className="flex items-center justify-center gap-2 border rounded-[40px] p-4 cursor-pointer" onClick={handleOpen}>
+        <div className="flex items-center justify-center gap-2 border rounded-[40px] p-4 cursor-pointer">
             <div className="avatar rounded-full min-h-12 min-w-12 bg-emerald-500 text-white flex item items-center font-[700] justify-center">
                 { sessionData?.user?.image ?
                     <p>
@@ -47,72 +25,6 @@ export default function UserItem(){
                 <p className="text-[16px] font-bold">{sessionData?.user?.name}</p>
                 <p className="text-[14px] text-neutral-500">{sessionData?.user?.email}</p>
             </div>
-            {isOpen && <DialogUserItem onClose={handleClose}/>}
         </div>
-    );
-}
-
-interface DialogUserItemProps {
-    onClose: () => boolean | undefined | void;
-}
-export function DialogUserItem({onClose}: DialogUserItemProps) {
-    const { data: sessionData } = useSession();
-
-    return (
-        <Dialog defaultOpen={true} onOpenChange={onClose}>
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogClose className="absolute right-4 top-4" onClick={onClose} />
-                <DialogHeader>
-                    <DialogTitle>Perfil</DialogTitle>
-                </DialogHeader>
-                <DialogBody>
-                    <div className="flex flex-col items-center">
-                        <div className="justify-self-center">
-                            <Avatar className={"min-h-[80px] min-w-[80px]"}>
-                                <AvatarImage className="rounded-[40px] min-h-2 min-w-2 object-cover"
-                                             src={sessionData?.user?.image ? sessionData?.user?.image : undefined}/>
-                                <AvatarFallback>{sessionData?.user?.name?.charAt(0)}</AvatarFallback>
-                            </Avatar>
-                        </div>
-                        <h4 className="font-bold text-lg mt-5"> {sessionData?.user?.name}</h4>
-                        <h3 className="font-bold">{sessionData?.user?.email}</h3>
-                        <DialogShareId/>
-                        <Button variant={"destructive"} className="min-w-32" onClick={() => signOut()}><LogOut className={"mr-2"}/>Sair</Button>
-                    </div>
-                </DialogBody>
-            </DialogContent>
-        </Dialog>
-    )
-}
-
-export function DialogShareId() {
-    const [shareId, setShareId] = useState(null as unknown as number);
-    const userMutation = api.user.generateShareId.useMutation();
-    useEffect(() => {
-        void userMutation.mutateAsync({}).then(id => {
-            if(id){
-                setShareId(id);
-            }
-        });
-        return () => {
-            setShareId(null as unknown as number);
-        }
-    }, []);
-    return (
-        <AlertDialog>
-            <AlertDialogTrigger><Button className="my-3">Gerar código de convite</Button></AlertDialogTrigger>
-            <AlertDialogContent className="flex-col flex items-center">
-                <AlertDialogHeader className="flex-col flex items-center">
-                    <AlertDialogTitle>Código de convite</AlertDialogTitle>
-                    <AlertDialogDescription className="flex-col flex items-center">
-                        Utilize esse código para estabeler conexão com outros usuários.
-                        <h1 className="font-bold text-xl">{shareId}</h1>
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogAction>Fechar</AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
     );
 }
